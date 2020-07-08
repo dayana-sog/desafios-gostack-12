679c99de3f9a564d4e26d5022fcdc52b8f0256d4
dayana-sog <dayanasog23@gmail.com>
2020-07-08 13:37:35 +0100

    Resolução do desafio 05

--- gostack-desafio-5/src/repositories/TransactionsRepository.ts
+++ gostack-desafio-5/src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((sum: number, transaction) => {
+      if (transaction.type === 'income') {
+        return sum + transaction.value;
+      }
+      return sum;
+    }, 0);
+
+    const outcome = this.transactions.reduce((sum: number, transaction) => {
+      if (transaction.type === 'outcome') {
+        return sum + transaction.value;
+      }
+      return sum;
+    }, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
