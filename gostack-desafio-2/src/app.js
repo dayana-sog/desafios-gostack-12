f03e92906e9c89fea5fd1f20c34de3e4f53f6815
dayana-sog <dayanasog23@gmail.com>
2020-07-08 12:40:52 +0100

    desafios 2, 6, 7

---
+++ gostack-desafio-2/src/app.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid, isUuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories)
+});
+
+app.post("/repositories", (request, response) => {
+  const {title, url, techs} = request.body
+  repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+  repositories.push(repository)
+  return response.status(201).json(repository)
+  
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const {id} = request.params
+  const {title, url, techs} = request.body
+  let {likes} = request.body
+  if (likes){
+    likes = 0
+  }
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  if(!isUuid(id)){
+    return response.status(400).json({error: 'ID is not uuid type.'})
+  }
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: 'Repository not found.'})
+  }
+  
+  likes = repositories[repositoryIndex].likes
+  
+  repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes,
+  }
+  repositories[repositoryIndex] = repository
+  return response.json(repository)
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const {id} = request.params
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  if(!isUuid(id)){
+    return response.status(400).json({error: 'ID is not uuid type.'})
+  }
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: 'Repository not found.'})
+  }
+
+  repositories.splice(repositoryIndex,1)
+
+  return response.status(204).send() 
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const {id} = request.params
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  if(!isUuid(id)){
+    return response.status(400).json({error: 'ID is not uuid type.'})
+  }
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: 'Repository not found.'})
+  }
+
+  repositories[repositoryIndex].likes += 1
+
+  
+
+  return response.json({
+    likes: repositories[repositoryIndex].likes
+  })
+
+});
+
+module.exports = app;
